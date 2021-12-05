8289ca3eabaf283ad1955af44772fead36499109
Matt Chun-Lum <matt@questionable.engineering>
2021-12-05 15:23:35 -0800

    perf: technically given the problem space, we can just re-use the previous runner

--- examples/004_giant_squid/main.rs
+++ examples/004_giant_squid/main.rs
@@ -4,14 +4,13 @@ use std::convert::TryFrom;
 fn main() {
     let lines = load_input("004").expect("could not load input");
     let mut runner: Runner<FastBoard> = Runner::try_from(lines).expect("Input was invalid");
-    let mut runner2 = runner.clone();
 
     println!(
         "part 1: {}",
         runner.play().expect("Could not find a winner")
     );
 
-    let score = runner2
+    let score = runner
         .par_find_last_scoring()
         .expect("Could not find the last winner");
     println!("part 2: {}", score);
